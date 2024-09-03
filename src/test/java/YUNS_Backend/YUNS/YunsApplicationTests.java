71382c1702967e9d4404519d609f0b684be6b5e2
dnwndls411 <dnwndls411@gmail.com>
2024-09-03 22:41:55 +0900

    초기 환경 세팅

---
+++ src/test/java/YUNS_Backend/YUNS/YunsApplicationTests.java
@@ -0,0 +1,13 @@
+package YUNS_Backend.YUNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
