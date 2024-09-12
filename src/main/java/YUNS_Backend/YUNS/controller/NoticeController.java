650e49988e4626b07affe60c1925e7c38464a68b
Hoo <b01066586906@gamil.com>
2024-09-12 17:56:35 +0900

    refact: 공지사항 작성 코드 변경

--- src/main/java/YUNS_Backend/YUNS/controller/NoticeController.java
+++ src/main/java/YUNS_Backend/YUNS/controller/NoticeController.java
@@ -1,5 +1,6 @@
 package YUNS_Backend.YUNS.controller;
 
+import YUNS_Backend.YUNS.dto.NoticeDto;
 import YUNS_Backend.YUNS.entity.Notice;
 import YUNS_Backend.YUNS.entity.User;
 import YUNS_Backend.YUNS.service.NoticeService;
@@ -10,7 +11,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,16 +24,16 @@ public class NoticeController {
     private UserService userService;
 
     @PostMapping("/api/admin/notices/create")
-    public ResponseEntity<Notice> createNotice(@RequestBody Notice notice, @AuthenticationPrincipal UserDetails currentUser) {
-        User user = userService.findUserByStudentNumber(currentUser.getUsername());
-        Notice newNotice = Notice.builder()
-                .title(notice.getTitle())
-                .content(notice.getContent())
-                .date(LocalDateTime.now())
-                .user(user)
-                .build();
-        return ResponseEntity.ok(noticeService.createNotice(newNotice));
-    }
+    public ResponseEntity<NoticeDto> createNotice(@RequestBody NoticeDto noticeDto, @AuthenticationPrincipal UserDetails currentUser) {
+      User user = userService.findUserByStudentNumber(currentUser.getUsername());
 
+        Notice newNotice = noticeService.convertToEntity(noticeDto);
+
+        // User 설정을 위해 updateUser 메서드 호출
+        newNotice = newNotice.updateUser(user);
+
+         NoticeDto createdNoticeDto = noticeService.createNotice(newNotice);
+        return ResponseEntity.ok(createdNoticeDto);
+    }
 
 }
