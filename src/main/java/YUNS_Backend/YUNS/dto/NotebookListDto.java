7c2e8ff01eec3a1f22827f977615dd93bf7825f4
dnwndls411 <dnwndls411@gmail.com>
2024-09-24 10:49:08 +0900

    feat: NotebookListDto 추가

---
+++ src/main/java/YUNS_Backend/YUNS/dto/NotebookListDto.java
@@ -0,0 +1,9 @@
+package YUNS_Backend.YUNS.dto;
+
+import YUNS_Backend.YUNS.entity.RentalStatus;
+
+public class NotebookListDto {
+    long notebookId;
+    String model;
+    RentalStatus rentalStatus;
+}
