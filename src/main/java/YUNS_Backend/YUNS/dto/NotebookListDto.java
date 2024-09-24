feb08a9fd70ef6b9fdbfd66ff32fc88233c13da5
dnwndls411 <dnwndls411@gmail.com>
2024-09-24 12:25:25 +0900

    feat: notebook 조회 관련 dto 추가

--- src/main/java/YUNS_Backend/YUNS/dto/NotebookListDto.java
+++ src/main/java/YUNS_Backend/YUNS/dto/NotebookListDto.java
@@ -1,9 +1,17 @@
 package YUNS_Backend.YUNS.dto;
 
 import YUNS_Backend.YUNS.entity.RentalStatus;
+import com.querydsl.core.annotations.QueryProjection;
 
 public class NotebookListDto {
     long notebookId;
     String model;
     RentalStatus rentalStatus;
+
+    @QueryProjection
+    public NotebookListDto(Long notebookId, String model, RentalStatus rentalStatus) {
+        this.notebookId = notebookId;
+        this.model = model;
+        this.rentalStatus = rentalStatus;
+    }
 }
