e72a32dd0e29d94ec6397c1b78a4fcb32bd3c434
Hoo <b01066586906@gamil.com>
2024-09-26 21:19:17 +0900

    feat: 전체 사용자 정보 리스트 조회, 세부 리스트 조회 구현

--- src/main/java/YUNS_Backend/YUNS/service/UserService.java
+++ src/main/java/YUNS_Backend/YUNS/service/UserService.java
@@ -5,12 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import YUNS_Backend.YUNS.entity.User;
 import YUNS_Backend.YUNS.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -62,5 +65,14 @@ public class UserService implements UserDetailsService {
         } else {
             throw new IllegalArgumentException("user가 존재하지 않습니다.");
         }
+    }
+
+
+    public Page<User> getAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    public Optional<User> findUserById(Long userId) {
+        return userRepository.findById(userId);
     }
 }
