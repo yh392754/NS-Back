579de55be1bcfeb21ddf49ad42a7b63e28813694
Hoo <b01066586906@gamil.com>
2024-10-06 00:22:35 +0900

    refact : 대여 현황 조회,수정,삭제 부분 제거

--- src/main/java/YUNS_Backend/YUNS/controller/NotebookController.java
+++ src/main/java/YUNS_Backend/YUNS/controller/NotebookController.java
@@ -118,49 +118,5 @@ public class NotebookController {
     }
 
 
-    //stauts 파라미터
-    @GetMapping("/api/admin/rentals/read")
-    public ResponseEntity<Page<NotebookListDto>> getNotebooksByRentalStatus(
-            @RequestParam("status") RentalStatus rentalStatus,
-            @RequestParam("page") Optional<Integer> page,
-            @RequestParam("size") Optional<Integer> size) {
-
-        Pageable pageable = PageRequest.of(page.orElse(0), size.orElse(10));
-        Page<NotebookListDto> notebooks = notebookService.getNotebooksByRentalStatus(rentalStatus, pageable);
-
-        return ResponseEntity.ok(notebooks);
-    }
-
-
-    //rentalStatus 파라미터
-    @PutMapping("/api/admin/rentals/{notebookId}/update")
-    public ResponseEntity<Object> updateRentalStatus(@PathVariable Long notebookId, @RequestParam RentalStatus rentalStatus) {
-
-        try {
-            notebookService.updateRentalStatus(notebookId, rentalStatus);
-        } catch (EntityNotFoundException e) {
-            throw new CustomException(ErrorCode.NOTEBOOK_NOT_FOUND);
-        }
-
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "노트북의 대여 상태가 성공적으로 수정되었습니다.");
-        return ResponseEntity.ok(response);
-    }
-
-    @DeleteMapping("/api/admin/rentals/{notebookId}/delete")
-    public ResponseEntity<Object> deleteNotebook(@PathVariable Long notebookId) {
-        Map<String, String> response = new HashMap<>();
-
-        try {
-            // 노트북 삭제 메서드 호출
-            notebookService.deleteNotebook(notebookId);
-            response.put("message", "노트북이 성공적으로 삭제되었습니다.");
-        } catch (EntityNotFoundException e) {
-            // 노트북을 찾지 못한 경우 예외 처리
-            throw new CustomException(ErrorCode.NOTEBOOK_NOT_FOUND);
-        }
-
-        return ResponseEntity.ok(response);
-    }
 
 }
