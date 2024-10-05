8d248f871ec46a2c534931195aa668b6169d255f
dnwndls411 <dnwndls411@gmail.com>
2024-10-06 06:35:49 +0900

    fix: 필드명 selectd -> selected 수정

--- src/main/java/YUNS_Backend/YUNS/dto/NotebookFilterDto.java
+++ src/main/java/YUNS_Backend/YUNS/dto/NotebookFilterDto.java
@@ -9,7 +9,7 @@ public class NotebookFilterDto {
     //사용자가 선택한 필터(크기/모델/선택안함)
     String filterBy;
     //사용자가 드롭다운에서 선택한 것
-    String selectd;
+    String selected;
     //대여가능한 노트북만 보기 선택 여부
     boolean onlyAvailable;
 
